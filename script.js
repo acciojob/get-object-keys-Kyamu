da0552ea1e553d772633a80ff7a3bdea7f0d61f3
Kyamu <124717126+Kyamu@users.noreply.github.com>
2023-04-20 23:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// Create the student object with a name property
+const student = {
+  name: 'John Doe'
+};
+
+// Add the getKeys method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Call the getKeys method on the student object to get all its keys
+const keys = student.getKeys();
+
+console.log(keys); // ["name"]
+
